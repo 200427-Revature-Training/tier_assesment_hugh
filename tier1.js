1b6ea4bdbd0ace6a6ae72516a32af45ffd6f21c0
Hugh Thornhill <hlowerthornhill@gmail.com>
2020-06-26 15:13:05 -0400

    Tier 1 complete

---
+++ tier1.js
@@ -0,0 +1,36 @@
+// Tier 1
+// 1.
+function returnDataTypeParameter(p) {
+    return typeof p;
+}
+
+// 2
+function returnProduct(...p) {
+    let product = 0;
+    // let product = p * i;
+    // console.log(product);
+    for(i = 0; i < p.length; i++) {
+        product *=p;
+    }
+    return product;
+}
+
+// 3
+function iterateOverInteger(arr) {
+    let sum = 0;
+    for(i = 0; i < p.length; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+// 4
+const sArray = (arr) => {
+    let string = arr.toString().split('');
+    let array = string.length;
+    let sum = 0;
+    string.forEach(i => {
+        sum+= string.Math.pow(i, array)
+    })
+    return sum == arr;
+}
